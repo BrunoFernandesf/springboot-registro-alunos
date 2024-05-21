f6dd75f6d383bd90fc963a9c8deb8651c18e7dcd
BrunoFernandesf <brunofernandesf.gd@gmail.com>
2024-05-21 10:32:07 -0300

    arquivos iniciais

---
+++ src/main/java/com/example/springbootregistroalunos/SpringbootRegistroAlunosApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootregistroalunos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRegistroAlunosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRegistroAlunosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springbootregistroalunos/SpringbootRegistroAlunosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootregistroalunos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRegistroAlunosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
